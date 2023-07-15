2b73bdf9cdd27ffef60870d873e6874731fa4edb
e.martirosian <e.martirosian@revolut.com>
2023-07-15 21:10:55 +0400

    Account and bucket

--- accounts.tf
+++ accounts.tf
@@ -11,3 +11,9 @@ resource "yandex_resourcemanager_folder_iam_member" "lusty-profile-sa_permission
 resource "yandex_iam_service_account_static_access_key" "lusty-profile-sa_static-key" {
   service_account_id = yandex_iam_service_account.lusty-profile-sa.id
 }
+
+resource "yandex_storage_bucket" "photo-bucket" {
+  access_key = lusty-profile-sa_static-key.access_key
+  secret_key = lusty-profile-sa_static-key.secret_key
+  bucket     = "photo-bucket"
+}
--- buckets.tf
+++
@@ -1,5 +0,0 @@
-resource "yandex_storage_bucket" "photo-bucket" {
-  access_key = lusty-profile-sa_static-key.access_key
-  secret_key = lusty-profile-sa_static-key.secret_key
-  bucket     = "photo-bucket"
-}
