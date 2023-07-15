7e80b2bc6a472ec72961ac82da4e41b5de79d624
e.martirosian <e.martirosian@revolut.com>
2023-07-15 21:13:59 +0400

    Account and bucket

--- main.tf
+++ main.tf
@@ -7,13 +7,6 @@ terraform {
   required_version = ">= 0.13"
 }
 
-provider "yandex" {
-  zone = "ru-central1-a"
-}
-
-# An example resource that does nothing.
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
-}
+//provider "yandex" {
+//  zone = "ru-central1-a"
+//}
