253f69aa6aa765e96c9a39c520b4a60ebb9ed216
e.martirosian <e.martirosian@revolut.com>
2023-07-15 21:03:52 +0400

    Account and bucket

---
+++ accounts.tf
@@ -0,0 +1,13 @@
+resource "yandex_iam_service_account" "lusty-profile-sa" {
+  name      = "lusty-profile-sa"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "lusty-profile-sa_permission_storage-editor" {
+  folder_id = "prod"
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.lusty-profile-sa.id}"
+}
+
+resource "yandex_iam_service_account_static_access_key" "lusty-profile-sa_static-key" {
+  service_account_id = yandex_iam_service_account.lusty-profile-sa.id
+}
---
+++ buckets.tf
@@ -0,0 +1,5 @@
+resource "yandex_storage_bucket" "photo-bucket" {
+  access_key = terraform-account.lusty-profile-sa_static-key.access_key
+  secret_key = account.lusty-profile-sa_static-key.secret_key
+  bucket = "photo-bucket"
+}
