84f4d1a3ba30a1cb71fc6c98c82fdaefb59e8785
e.martirosian <e.martirosian@revolut.com>
2023-07-16 04:15:01 +0400

    Account and bucket

--- accounts.tf
+++ accounts.tf
@@ -6,8 +6,13 @@ resource "yandex_iam_service_account_static_access_key" "lusty-profile-sa_static
   service_account_id = yandex_iam_service_account.lusty-profile-sa.id
 }
 
-resource "yandex_resourcemanager_folder_iam_member" "lusty-profile-sa-bucket-permission" {
+resource "yandex_resourcemanager_folder_iam_member" "lusty-profile-sa_permission_storage-editor" {
   folder_id = local.folder_id
   role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.lusty-profile-sa.id}"
+}
+resource "yandex_resourcemanager_folder_iam_member" "lusty-profile-sa_permission_storage-uploader" {
+  folder_id = local.folder_id
+  role      = "storage.uploader"
   member    = "serviceAccount:${yandex_iam_service_account.lusty-profile-sa.id}"
 }
